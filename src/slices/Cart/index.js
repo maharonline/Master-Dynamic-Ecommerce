28b095fbef1fbe940a9c832cd6d6c07335816bde
Hamza Ahmad <132140231+maharonline@users.noreply.github.com>
2025-11-17 11:17:27 +0500

    Update index.js

--- src/slices/Cart/index.js
+++ src/slices/Cart/index.js
@@ -1,4 +1,4 @@
-"use-client"
+"use client"
 import { useEffect, useState } from "react";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText } from "@prismicio/react";
